5add380dacfb30ba0f94d86ad9e308e2c414bde9
zhentaoumich <117057647+zhentaoumich@users.noreply.github.com>
2023-08-08 02:44:54 +0800

    Update wow.py

--- wow.py
+++ wow.py
@@ -15,7 +15,7 @@ st.text('Webapp by Team 12 Hardcore')
 
 #displaying the image on streamlit app
 
-placeholder=st.image("https://boosting.pro/wp-content/uploads/2019/08/WoW-Classic-Vanilla-Map.jpg")
+#placeholder=st.image("https://boosting.pro/wp-content/uploads/2019/08/WoW-Classic-Vanilla-Map.jpg")
 
 
 
